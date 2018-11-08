b494823c66722b37412bf854055d441e1dae2f55
timronan <tronan@iris.washington.edu>
2018-11-07 16:18:33 -0800

    Added Test which determine converter output of left vs right justification

--- src/test/java/edu/iris/dmc/station/converter/XmlToSeedFileConverterTest.java
+++ src/test/java/edu/iris/dmc/station/converter/XmlToSeedFileConverterTest.java
@@ -1,6 +1,7 @@
 package edu.iris.dmc.station.converter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.io.File;
 import java.util.List;
@@ -29,8 +30,55 @@ public class XmlToSeedFileConverterTest {
 		assertEquals(2, list.size());
 
 		B050 anmo = list.get(0);
+		
+		System.out.println(anmo.getStationCode());
 		assertEquals("1989,241,00:00:00.0000",anmo.getStartTime().toSeedString());
+		
+
+	}
+	
+	@Test
+	public void t2() throws Exception {
+
+		File xml = new File(XmlToSeedFileConverterTest.class.getClassLoader().getResource("IU_ANMO_BHZ.xml").getFile());
+
+		File convertedSeedFile = new File("converted.dataless");
+		XmlToSeedFileConverter.getInstance().convert(xml, convertedSeedFile);
+
+		Volume volume = SeedUtils.load(convertedSeedFile);
+
+		List<B050> list = volume.getB050s();
+		assertEquals(2, list.size());
+
+		B050 anmo = list.get(0);
+		
+		
+		assertNotEquals(" ANMO", anmo.getStationCode());
+		
+
+	}
+	
+	@Test
+	public void t3() throws Exception {
+
+		File xml = new File(XmlToSeedFileConverterTest.class.getClassLoader().getResource("IU_ANMO_BHZ.xml").getFile());
+
+		File convertedSeedFile = new File("converted.dataless");
+		XmlToSeedFileConverter.getInstance().convert(xml, convertedSeedFile);
+
+		Volume volume = SeedUtils.load(convertedSeedFile);
+
+		List<B050> list = volume.getB050s();
+		assertEquals(2, list.size());
+
+		B050 anmo = list.get(0);
+		
+		
+		assertEquals("ANMO ", anmo.getStationCode());
+		
 
 	}
 
 }
+
+
