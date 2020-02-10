cef2666c5fcde9eb101f1ad00e4c415942bb184c
timronan <tronan@iris.washington.edu>
2020-02-10 10:10:20 -0800

    Update normalization mapper test to work with issue #43

--- src/test/java/edu/iris/dmc/station/mapper/NormalizationMapper.java
+++ src/test/java/edu/iris/dmc/station/mapper/NormalizationMapper.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.EOFException;
 import java.io.File;
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
@@ -29,23 +31,37 @@ import edu.iris.dmc.station.converter.XmlToSeedDocumentConverterTest;
 public class NormalizationMapper {
 
 
+@SuppressWarnings("null")
 @Test
 public void normilization_frequency() throws Exception {
 	File source = new File(
 			XmlToSeedDocumentConverterTest.class.getClassLoader().getResource("UW.LON.dataless.20200204125001").getFile());
 
 	Volume original = IrisUtil.readSeed(source);
-	B053 pz = null;
+	 B053 pz = null;
+	 List <B053> pzlist = null;
 	 B050 datasta = original.getB050s().get(0);
 	 List <B052> datachan = datasta.getB052s();
-	 for (int i2=1; i2 < datachan.size(); i2++) {
+	 try {
+	 for (int i2=0; i2 < datachan.size(); i2++) {
+		// if(datachan.get(i2).getResponseStages().get(0).getBlockettes().get(0)==null) {
+	
+		 //}else {
+		 // pzlist.add(i2, (B053) datachan.get(i2).getResponseStages().get(0).getBlockettes().get(0));
+		// }
 		  if(datachan.get(i2).getChannelCode().matches("SNE")) {
 			  pz = (B053) datachan.get(i2).getResponseStages().get(0).getBlockettes().get(0);
-			  System.out.println(pz.getNormalizationFactor());
+			 // System.out.println(pz.getNormalizationFactor());
 		  }
 	 }
-		  
-
+	 
+	 }catch (Exception e) {
+         /* This is a generic Exception handler which means it can handle
+          * all the exceptions. This will execute if the exception is not
+          * handled by previous catch blocks.
+          */
+         System.out.println("Exception occurred first loop");
+      }		  
 	FDSNStationXML document = SeedToXmlDocumentConverter.getInstance().convert(original);
 	
 	
@@ -55,20 +71,31 @@ public void normilization_frequency() throws Exception {
     Marshaller marshallObj = jContext.createMarshaller();
     //setting the property to show xml format output
     marshallObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-    //setting the values in POJO class
 
-    //calling the marshall method
-    //marshallObj.marshal(document, System.out);
     Network net = document.getNetwork().get(0);
     Station sta = net.getStations().get(0);
     Response res = null;
+    
+    ///TODO FInd the null pointer and remove it. 
     List<Channel> cha  = sta.getChannels();
-    for (int i=1; i < cha.size(); i++) {
-      if(cha.get(i).getCode().matches("SNE")) {
+    System.out.println(cha.size());
+    int n=0;
+    try {
+    for (int i=0; i < cha.size(); i++) {
+    	if(cha.get(i).getCode().matches("SNE")) {
     	  res = cha.get(i).getResponse();
-    	  
+    	   
       }
     }
-    assertEquals(res.getStage().get(0).getPolesZeros().getNormalizationFactor().doubleValue(), pz.getNormalizationFactor(), 0.1);
+	}catch (Exception e) {
+         /* This is a generic Exception handler which means it can handle
+          * all the exceptions. This will execute if the exception is not
+          * handled by previous catch blocks.
+          */
+         System.out.println("Exception occurred second loop");
+     }
+    System.out.println(res.getStage().get(0).getPolesZeros().getNormalizationFactor());
+    assertEquals(res.getStage().get(0).getPolesZeros().getNormalizationFactor(), pz.getNormalizationFactor(), 0.1);
+    assertEquals(Double.toString(pz.getNormalizationFactor()).length(), 9);
 }
 }
